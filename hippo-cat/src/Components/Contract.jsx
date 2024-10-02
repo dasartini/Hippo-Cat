854daa70e55f9c9409223a4a68ad000c5f2c0b1d
dasartini <adriansartinid@gmail.com>
2024-10-02 22:24:52 +0100

    finished about component

--- hippo-cat/src/Components/Contract.jsx
+++ hippo-cat/src/Components/Contract.jsx
@@ -1,14 +1,15 @@
 import HeadBarStyle from "../Styles/HeadBarStyle";
 
-function Contract(){
-return(
-    <HeadBarStyle>
-<div className="cardStyle">
-    CA: A9jLULrDSYZkBtKfvG44w8NWzvCN6rGLpoA3hY7TQ85k
-</div>
+function Contract() {
+    return (
+        <HeadBarStyle>
 
-    </HeadBarStyle>
-)
+            <div className="cardStyle">
+                CA: 8gsPhwLeVNQe6NNi42T6Q2n2q3ZPFwiaJwrk7CzaKhLC
+            </div>
+
+        </HeadBarStyle>
+    )
 
 }
 
--- hippo-cat/src/Components/Hero.jsx
+++ hippo-cat/src/Components/Hero.jsx
@@ -1,6 +1,7 @@
 import HeroStyle from "../Styles/HeroStyle";
 import hippoCat from '../assets/hippo.jpeg'
 import tweet from '../assets/tweet.jpeg'
+import moo from '../assets/moo.jpeg'
 function Hero(){
 
     return(
@@ -8,9 +9,11 @@ function Hero(){
             <div className="heroContainer">
         <img className='hippoCat' src={hippoCat}/>
         <div className="buttonsContainer">
-        <button className='buy'> BUY NOW</button>
-        <button className="chart">Chart</button>
+        <a href="https://raydium.io/swap/?inputMint=sol&outputMint=8gsPhwLeVNQe6NNi42T6Q2n2q3ZPFwiaJwrk7CzaKhLC" target="_blank"><button className='buy'> BUY NOW</button></a>
+        <a href="https://www.dextools.io/app/en/solana/pair-explorer/98gsPhwLeVNQe6NNi42T6Q2n2q3ZPFwiaJwrk7CzaKhLC" target="_blank">
+        <button className="chart">Chart</button></a>
         </div>
+        <img className="moo" src={moo}/>
         <h1> First Hippo Cat coin on Pump.fun</h1>
         <img className='tweet' src={tweet}/>
         </div>
